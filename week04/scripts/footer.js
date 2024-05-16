92135b6b476e00cc75056e0d012da3f71e991d97
Anton Gorokhov <bonduelle85@gmail.com>
2024-05-17 00:30:18 +0300

    week 4 activities

---
+++ week04/scripts/footer.js
@@ -0,0 +1,2 @@
+document.querySelector("#currentYear").innerHTML = `&copy ${new Date().getFullYear()}`
+document.querySelector("#lastModified").innerHTML = `Last Modification: ${document.lastModified}`
